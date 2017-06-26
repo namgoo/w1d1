6125b89b8b0aef419827e28e8df1e0f29b1271bc
Nam Yoo <namgooy@gmail.com>
2017-06-26 20:18:56 +0000

    Final

--- add_numbers.js
+++ add_numbers.js
@@ -5,23 +5,29 @@
 
 var args = process.argv.slice(2);
 
-console.log(process.argv);
-var number1 = parseInt(process.argv[0]);
-var number2 = parseInt(process.argv[1]);
+
+var number1 = parseInt(args[0]);
+var number2 = parseInt(args[1]);
 
 // console.log(number1);
 // console.log(number2);
 
 // console.log(typeof number1);
 
+// var sum = number1 + number2;
+// console.log(sum);
+
 if (Number.isNaN(number1)
   || Number.isNaN(number2)) {
   console.log("Input was not a number.")
 } else {
-  var sum = number1 + number2;
-  cosnole.log(sum);
+  var result = 0;
+  for (var i = 0; i < args.length; i++) {
+    console.log(args[i])
+    result = result +Number(args[i]);
+  }
+    console.log(result)
 }
 
 
-var sum = number1 + number2;
-console.log(sum);
+//
